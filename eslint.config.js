55524fcf9ea4efed7f02978f74abc3bf2ff3f98d
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-09-28 10:31:19 +0200

    Remove browser globals from ESLint config (#12)
    This code is meant to run on non-browser environments, so not only having these globals configured is unnecessary, but also potentially harmful, as one may accidentally use some browser global and ESLint would be happy about it.
    
    Co-authored-by: James Garbutt <43081j@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import eslintjs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import {defineConfig} from 'eslint/config';
-import globals from "globals";
 
 export default defineConfig([
   {
@@ -9,11 +8,6 @@ export default defineConfig([
     plugins: {
       eslint: eslintjs,
       typescript: tseslint
-    },
-    languageOptions: {
-      globals: {
-        ...globals.browser
-      }
     },
     extends: [
       tseslint.configs.strict,
